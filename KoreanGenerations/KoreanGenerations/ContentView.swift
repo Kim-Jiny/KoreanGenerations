483038c70822134cd50a8a1b502927db7ccbb9ac
Jiny <jiny@nachocode.io>
2025-04-16 11:25:12 +0900

    first project

---
+++ KoreanGenerations/KoreanGenerations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KoreanGenerations
+//
+//  Created by 김미진 on 4/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KoreanGenerations/KoreanGenerations/KoreanGenerationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoreanGenerationsApp.swift
+//  KoreanGenerations
+//
+//  Created by 김미진 on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct KoreanGenerationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
